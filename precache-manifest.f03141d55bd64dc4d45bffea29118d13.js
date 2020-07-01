688cfbbd18e65a3ce8dada734d01e3123ba2bdc2
adelmoralm <adelmoralm@bitbucket.org>
2020-06-30 21:40:09 -0500

    Updates

---
+++ precache-manifest.f03141d55bd64dc4d45bffea29118d13.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c061150f81ae64339ffdc33cbf89bdb",
+    "url": "/ALMALEVISOL/bellotero.io.git/index.html"
+  },
+  {
+    "revision": "5e88697fed327d31eb75",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/css/2.8c6566a3.chunk.css"
+  },
+  {
+    "revision": "988a20fd89e07c178048",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/css/main.ba21cd00.chunk.css"
+  },
+  {
+    "revision": "5e88697fed327d31eb75",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/js/2.19d10285.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/js/2.19d10285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988a20fd89e07c178048",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/js/main.a6fdac61.chunk.js"
+  },
+  {
+    "revision": "77316246e68665aba9bd",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/js/runtime-main.ebcd8f18.js"
+  },
+  {
+    "revision": "1a00134f7baf6a4783dc1135145a361b",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/media/Roboto-Black.1a00134f.ttf"
+  },
+  {
+    "revision": "36b5bab58a18b9c924861a4ccbf1a790",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/media/Roboto-Bold.36b5bab5.ttf"
+  },
+  {
+    "revision": "d5c46b134c17b2138dcd1bb0efa67049",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/media/Roboto-Light.d5c46b13.ttf"
+  },
+  {
+    "revision": "9745415b8a2a8b9a2480a12f7e2d3ad3",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/media/Roboto-Medium.9745415b.ttf"
+  },
+  {
+    "revision": "2e7b33965fa14b8b259536a3718dcb39",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/media/bellotero.2e7b3396.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ALMALEVISOL/bellotero.io.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
